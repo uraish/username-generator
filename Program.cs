74ce868c55c5964e45b8834d61f56e9a1df76f9f
uraishahmad <46197878+uraishahmad@users.noreply.github.com>
2021-11-21 15:56:42 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Username_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the username.");
+            string username = Console.ReadLine();
+            DateTime date;
+            Console.WriteLine("Please enter your date of birth in DD/MM/YYYY format.\n");
+            var dob = Console.ReadLine();
+
+            if (DateTime.TryParseExact(dob.ToString(), "d/M/yyyy",CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            {
+                Console.WriteLine("Your username is: " + username.ToLower() + date.ToString("ddMMyyyy"));
+            }
+            else
+            {
+                Console.WriteLine("Invalid date of birth");
+            }
+        }
+    }
+}
